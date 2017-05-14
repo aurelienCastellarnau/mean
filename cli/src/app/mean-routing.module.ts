ef2232f573d1a5d2b6c3a4272b4c1ecd96102968
aurelienCastellarnau <castellarnau.a@gmail.com>
2017-05-14 14:42:00 +0200

    [client] ROUTING module tout seul... qui centralise le routing Angular2.

---
+++ cli/src/app/mean-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CaseDetailComponent }  from './components/case-detail.component';
+import { CasesComponent }       from './components/cases.component';
+import { SearchCaseComponent }  from './components/search-case.component';
+
+/*
+** Ce fichier centralise le routing
+** on pourrait envisager un fichier
+** contenant les routes mais
+** tout mettre ici me parait bien!
+*/
+const routes: Routes = [
+  {
+    path: 'search',
+    component: SearchCaseComponent
+  },
+  {
+    path: 'search/:param',
+    component: SearchCaseComponent
+  },
+  {
+    path: 'cases/:param',
+    component: CaseDetailComponent
+  },
+  {
+    path: 'cases',
+    component: CasesComponent
+  },
+];
+
+/*
+** @NgModule: déclaration d'un module, grâce à l'import NgModule
+** le module importe le router module d'angular, set nos routes
+** et l'exporte.
+*/
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class MeanRoutingModule {}
