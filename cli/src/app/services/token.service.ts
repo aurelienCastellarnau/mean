45cc601e90f78075074982466d4214f67df28549
Kumatetsu <billaudjean@gmail.com>
2017-05-18 09:03:57 +0200

    Commencement token

--- cli/src/app/services/token.service.ts
+++ cli/src/app/services/token.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable }             from '@angular/core';
+import { Http }                   from '@angular/http';
+import { ErrorHandlerService }    from '../services/error-handler.service';
 
 @Injectable()
-export class TokenService{}
+export class TokenService{
+    constructor(private http: Http, private handleError: ErrorHandlerService) {}
+
+    Login(agentCode: string, password: string): Promise<any> {
+
+    }
+}
