dc8df924de52f861961f1cf87a9754dff11ecce7
aurelienCastellarnau <castellarnau.a@gmail.com>
2017-06-06 00:13:18 +0200

    [api] ADD argument -index au programme doit être à true pour lancer l'index elastic search ADD commandes npm run deploy_win_index et npm run deploy_unix_index

--- api.js
+++ api.js
@@ -20,13 +20,25 @@ db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function () {
     console.log('connection succeeded');
     app.use(express.static(path.join(conf.clipath)));
+
     /*
     ** Appel du service d'indexation en dehors du flow des
     ** middlewares. Comme ca on l'appelle qu'une fois...
+    ** De plus, l'api bénéficie maintenant de l'argument:
+    ** -index [true|false] => doit être à true pour lancer l'index
+    ** Ajout des commandes npm run deploy_win_index et deploy_unix_index
     ** Reste à implémenter la maj du modèle elasticSearch
     ** => update de l'index 'cases'.
     */
-    ESindex.init();
+    process.argv.forEach(function (val, index, array) {
+        let argExist = (array[index + 1]) ? true : false
+        let indexTrue = false;
+        if (argExist)
+            indexTrue = (array[index + 1] === 'true')
+        if (val === '-index' && indexTrue) {
+            ESindex.init();
+        }
+    });
     app.get('/*', (req, res) => {
         res.sendFile(path.join(conf.clipath, 'index.html'));
     });
