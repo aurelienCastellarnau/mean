7b6ff5c66080273053a568dcec9ca8e80faf2fb0
aurelienCastellarnau <castellarnau.a@gmail.com>
2017-06-05 23:01:45 +0200

    [api] recursiv indexation by 5000 slices

--- services/elastic.js
+++ services/elastic.js
@@ -9,20 +9,26 @@ const elastic = {
     "init": function init() {
         Cases.find({}, { '_id': 0 }, function (err, c) {// { '_id': 0 },
             if (err) {
-               // console.log("[ES in /cases routes] indexation error: ", err)
+                // console.log("[ES in /cases routes] indexation error: ", err)
             } else {
-                for (let key in c) {
-                    ESClient.index({
-                        index: 'cases',
-                        id: key,
-                        type: 'cases',
-                        body: c[key]
-                    }, function (err, resp, status) {
-                      //  console.log("[ElasticSearch Cases indexation: ", resp)
-                    });
-                }
+                index(c, 5000)
             }
-        }).limit(100)
+        }).limit(22000)
+    }
+}
+function index(c, limit, last) {
+    if (!last)
+        last = 0
+    for (let key = last; key < c.length && key < limit; key++) {
+        ESClient.index({
+            index: 'cases',
+            id: key,
+            type: 'cases',
+            body: c[key]
+        }, function (err, resp, status) {
+            if (key === limit - 1 && key !== (c.length - 1))
+                index(c, limit + 5000, key)
+        });
     }
 }
 module.exports = elastic
