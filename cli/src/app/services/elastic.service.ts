a39217c3aa99afefc5514a30479c4bb100f5819d
aurelienCastellarnau <castellarnau.a@gmail.com>
2017-06-05 21:37:14 +0200

    [client] service de requêtes à l'API elasticsearch/

--- cli/src/app/services/elastic.service.ts
+++ cli/src/app/services/elastic.service.ts
@@ -37,14 +37,50 @@ export class ElasticService {
             .catch(this.handleError.handlePromise)
     }
 
-    getCase(param: string): Promise<Case>{
+    getCase(param: String): Promise<Case>{
         const url = `/elasticsearch/${param}`
         const that = this
 
         return this.http.get(url, this.jwt())
         .toPromise()
-        .then(response => this.case = response.json() as Case)
+        .then(response => response.json() as Case)
         .catch(this.handleError.handlePromise)
+    }
+
+    /*
+    ** De ce côté ca marche comme ailleurs...
+    */
+    search(param: String): Promise<any>{
+        const url = `/elasticsearch/${param}`
+        const that = this
+
+        return this.http.get(url, this.jwt())
+        .toPromise()
+        .then(response => response.json())
+        .catch(this.handleError.handlePromise)
+    }
+
+    /*
+    ** particularité de la réponse fournie par elastic search.
+    ** les objets sont dans response.hits.hits[
+    **                                        0 => [
+    **                                            _source: case
+    **                                        ]
+    **                                        1 => [
+    **                                            _source: case
+    **                                        ]
+    **                                        etc...
+    **                                  ]
+    ** d'où cette méthode de parsing.
+    */
+    toCases(elasticObject: any): Case[]{
+        let cases = []
+
+        elasticObject.hits.hits.forEach(hit => {
+            cases.push(hit._source)
+        })
+        console.log(cases)
+        return cases
     }
 
     private jwt() {
