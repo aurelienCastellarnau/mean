eaf5621bace09e7f21008cc7b9395d56e46bbfdd
aurelienCastellarnau <castellarnau.a@gmail.com>
2017-06-05 21:38:59 +0200

    [client] component maitre de la partie elastic. C'est ce composant dont je me sers pour stocker le modèle et faire descendre les variables C'est ni plus ni moins qu'un héritagle, avec les méthodes codées dans ce component et ainsi accesssible dans tout les templates enfants.

--- cli/src/app/components/elastic/elastic-browser.component.ts
+++ cli/src/app/components/elastic/elastic-browser.component.ts
@@ -1,11 +1,46 @@
-import { Component, Input } from '@angular/core';
-import { Case }             from '../../model/case'
+import { Component, 
+         Input,
+         OnInit }               from '@angular/core';
+import { ElasticCasesComponent} from './elastic-cases.component';
+import { Case }                 from '../../model/case';
+import { CaseService }          from '../../services/case.service';
+import { ElasticService }       from '../../services/elastic.service';
+import { ErrorHandlerService }  from '../../services/error-handler.service';
 
 @Component({
     selector: 'elastic-browser',
     templateUrl: '../../templates/elastic/elastic-browser.component.html',
 })
-export class ElasticBrowserComponent {
-    public selectedCase: Case
-    @Input() cases: Case[]
+export class ElasticBrowserComponent implements OnInit{
+    public   selectedCase:    Case
+    public   properties:      String[]
+    public   query:           String
+    public   cases:           Case[]
+
+    constructor(
+        private elastic:      ElasticService,
+        private caseService:  CaseService,
+        private errorHandler: ErrorHandlerService,
+        ){}
+    
+    search(): any {
+        const that = this
+
+        console.log(this.query)
+        this.elastic.search(this.query)
+        .then(function(results){
+            that.cases = that.elastic.toCases(results)
+            console.log(that.cases)
+        })
+        .catch(this.errorHandler.handlePromise)
+    }
+
+    ngOnInit(): void {
+        this.elastic.getCases()
+        .then(cases => this.cases = cases)
+        .catch(this.errorHandler.handlePromise)
+        this.caseService.getProperties()
+        .then(prop => this.properties = prop)
+        .catch(this.errorHandler.handlePromise)
+    }
 }
