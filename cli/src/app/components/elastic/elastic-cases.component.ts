195e33e59e909a354a88f9872a33d536421a4303
aurelienCastellarnau <castellarnau.a@gmail.com>
2017-06-05 21:40:40 +0200

    [api] Add component pour gérer le display all full elastic search le GET all est une requête au serveur elastic et la première recherche est en place. ce template implémente case-edit et fait descendre le case et les properties.

---
+++ cli/src/app/components/elastic/elastic-cases.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Case }             from '../../model/Case';
+
+@Component({
+    selector: 'elastic-cases',
+    templateUrl: '../../templates/elastic/elastic-cases.component.html',
+})
+
+export class ElasticCasesComponent {
+    /*
+    ** le @Input nous permet de lié la variable
+    ** à un component parent et de la transmettre
+    ** lors de l'appel du template 
+    ** (voir elastic-browser.component.html et l'appel de <elastic-cases>)
+    */
+    @Input() cases:        Case[]
+    @Input() properties:   String[]
+    public   selectedCase: Case
+
+    selectCase(c: Case): void {
+        this.selectedCase = c
+        console.log(this.selectedCase)
+    }
+}
